25129d94de9038c770b3bd2c2daadd3762c3c8d2
SSFSKIM <kimmi@ssfs.org>
2023-10-25 21:40:55 +0900

    kjj

--- guess.py
+++ guess.py
@@ -4,7 +4,10 @@ import random
 
 a = random.randint(1, 10)
 b = int(input('guess the number'))
-if a == b:
-    print('congratulations!')
-else:
-    print(f'oh sorry. it was {abs(b-a)} away from the answer: {a}')
+while True:
+    if a == b:
+        print('congratulations!')
+        break
+    else:
+        print(f'oh sorry. it was {abs(b-a)} away from the answer')
+        b = int(input('guess the number'))
--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,9 +1,30 @@
-def who_wins(user, computer):
-    # 1 is rock, 2 is paper, 3 is scissors
-    pass
+#Steve
+#10.25
 
+import random
+def who_wins(u, c):
+    # 1 is rock, 2 is paper, 3 is scissors
+    while True:
+        if u == 3 and c == 1 or u == 1 and c==3:
+            return min(u, c)
+        elif u == c:
+            return 0
+        else:
+            return max(u, c)
+
+    pass
+dic = {1: 'rock', 2: 'paper', 3: 'scissor' }
 
 def main():
+    a = int(input())
+    b = random.randint(1,3)
+    who_wins(a, b)
+    if who_wins(a, b) == a:
+        print(f'user win (user:{dic[a]} vs computer:{dic[b]})')
+    elif who_wins(a, b) == 0:
+        print('it\'s a tie')
+    else:
+        print(f'computer win (user: {dic[a]} vs computer: {dic[b]})')
     pass
 
 
